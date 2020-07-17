0e3c3b4a690a76aa461ea96d67d96ecaded1dc10
congtung10t2 <congtung10t2@gmail.com>
2020-07-17 17:35:54 +0700

    Add login screen feature

---
+++ TwitterDemo/Screens/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  TwitterDemo
+//
+//  Created by TungHC on 7/17/20.
+//  Copyright © 2020 Tunghc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
