3d4411c6e3acb0f3a96be22b2cc5e3cd436e00e8
congtung10t2 <congtung10t2@gmail.com>
2020-07-17 16:37:03 +0700

    Init project with structured

---
+++ TwitterDemo/Constants/AppConstants.swift
@@ -0,0 +1,11 @@
+//
+//  AppConstants.swift
+//  TwitterDemo
+//
+//  Created by TungHC on 7/17/20.
+//  Copyright © 2020 Tunghc. All rights reserved.
+//
+
+struct AppConstants {
+    private init() {}
+}
---
+++ TwitterDemo/Screens/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterDemo
+//
+//  Created by TungHC on 7/17/20.
+//  Copyright © 2020 Tunghc. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
